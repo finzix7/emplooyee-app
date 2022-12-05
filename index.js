8f66e3b4ef4b96306d5400a482c60b37ee82f0d5
Diego Oliver <diegooliver.globant@latam.com>
2022-12-05 14:06:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { dbConnection } = require('./databases/config');
+const cors = require('cors');
+require('dotenv').config();
+
+//Crear el servidor express
+const app = express();
+
+//Base datos
+dbConnection();
+
+//CORS
+app.use(cors());
+
+//Diirectorio Publico
+app.use(express.static('public'));
+
+//Lectura y parse del body
+app.use(express.json());
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/colaboradores', require('./routes/colaboradores'));
+
+//Coreccion de ruta
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+//escuhcar peticiones
+app.listen(port = process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
